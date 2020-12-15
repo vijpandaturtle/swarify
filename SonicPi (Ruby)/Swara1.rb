f899b69a209d42a18833da85a232f25343157c20
Vijayasri <thisisvij98@gmail.com>
2020-12-15 20:10:28 +0530

    Added raaga swaras in ruby

---
+++ SonicPi (Ruby)/Swara1.rb
@@ -0,0 +1,84 @@
+#Swara Dictionary
+Sa = :C3
+ReKomal = :Db3
+Re = :D3
+GaKomal = :Eb3
+Ga = :E3
+Ma = :F3
+MaTeevra = :Fs3
+Pa = :G3
+DhaKomal = :Ab3
+Dha = :A3
+NiKomal = :Bb3
+Ni = :B3
+UpperSa = :C4
+
+#Setting tempo
+use_bpm 150
+
+#Defining different raga structures
+#MayaMalavaGowla/Bhairav
+play Sa
+sleep 0.5
+play ReKomal
+sleep 0.5
+play Ga
+sleep 0.5
+play Ma
+sleep 0.5
+play Pa
+sleep 0.5
+play DhaKomal
+sleep 0.5
+play Ni
+sleep 0.5
+play UpperSa
+sleep 2.0
+
+#Bhoop/Mohanam
+play Sa
+sleep 0.5
+play Re
+sleep 0.5
+play Ga
+sleep 0.5
+play Pa
+sleep 0.5
+play Dha
+sleep 0.5
+play UpperSa
+sleep 2.0
+
+#Shankarabharanam/Bilawal
+play Sa
+sleep 0.5
+play Re
+sleep 0.5
+play Ga
+sleep 0.5
+play Ma
+sleep 0.5
+play Pa
+sleep 0.5
+play Dha
+sleep 0.5
+play Ni
+sleep 0.5
+play UpperSa
+sleep 2.0
+
+#Abogi
+play Sa
+sleep 0.5
+play Re
+sleep 0.5
+play GaKomal
+sleep 0.5
+play Ma
+sleep 0.5
+play Dha
+sleep 0.5
+play UpperSa
+sleep 2.0
+
+
