5b52729e3bbd105f409a5547103066d5590a7e6a
Vijayasri Iyer <thisisvij98@gmail.com>
2021-05-18 21:31:09 +0530

    Auto-update metronome BPM

--- swarify/static/app.js
+++ swarify/static/app.js
@@ -2,30 +2,28 @@ var metronome = new Metronome();
 var tempo = document.getElementById('tempo');
 var tempoDisplayElem = document.getElementById('tempo-display');
 
-updateDisplay();
 
 function updateDisplay(){
-    tempoDisplayElem.innerHTML = metronome.tempo + 'BPM';
+    tempoDisplayElem.innerHTML = metronome.tempo + ' ' + 'BPM';
 };
+
+updateDisplay();
 
 var playButton = document.getElementById('play-button');
 playButton.addEventListener('click', function() {
     metronome.start();
-    updateDisplay();
 });
 
 var pauseButton = document.getElementById('pause-button');
 pauseButton.addEventListener('click', function() {
     metronome.stop();
-    updateDisplay();
 });
 
 var tempoChangeButtons = document.getElementsByClassName('tempo-change');
 for (var i = 0; i < tempoChangeButtons.length; i++) {
     tempoChangeButtons[i].addEventListener('click', function() {
         metronome.tempo += parseInt(this.dataset.change);
-        tempo.textContent = metronome.tempo;
+        tempoDisplayElem.innerHTML = metronome.tempo + ' ' + 'BPM';
     });
-    updateDisplay();
 }
 
